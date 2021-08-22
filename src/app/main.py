6c860fe74dd7a7c33b4310c38b82577ebabe9d53
lucapitzoi <luca.pitzoi@sindria.org>
2021-08-22 12:00:08 +0200

    Fix local env

--- src/app/main.py
+++ src/app/main.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
+
 import helpers
 
-#import pydevd_pycharm
-#pydevd_pycharm.settrace('10.10.100.1', port=4200, stdoutToServer=True, stderrToServer=True)
+# Pycharm pydevd
+PYCHARM_PYDEVD_ENABLED = int(os.getenv('PYCHARM_PYDEVD_ENABLED'))
+PYCHARM_PYDEVD_HOST = str(os.getenv('PYCHARM_PYDEVD_HOST'))
+PYCHARM_PYDEVD_PORT = int(os.getenv('PYCHARM_PYDEVD_PORT'))
+
+# Enable pydevd debugger
+if (PYCHARM_PYDEVD_ENABLED):
+    import pydevd_pycharm
+    pydevd_pycharm.settrace(PYCHARM_PYDEVD_HOST, port=PYCHARM_PYDEVD_PORT, stdoutToServer=True, stderrToServer=True)
 
 # Sample lightsail
 #aws lightsail create-instances-from-snapshot --instance-snapshot-name WordPress-1-1569866208 --instance-names WordPress-2 --availability-zone us-west-2a --bundle-id medium_2_0
 
 # Sample ec2
-#aws ec2 create-instances-from-snapshot --instance-snapshot-name WordPress-1-1569866208 --instance-names WordPress-2 --availability-zone us-west-2a --bundle-id medium_2_0
+#aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-903004f8 --subnet-id subnet-6e7f829e
 
 
 
@@ -18,15 +27,15 @@ import helpers
 def main():
     data = helpers.app()
 
-    # Process lightsail deployments
     for k,immutable in data['immutables'].items():
+        # Process lightsail deployments
         if (immutable['type'] == "lightsail"):
             subprocess.call(['aws', 'lightsail','create-instances-from-snapshot', '--instance-snapshot-name', immutable['refer'], '--instance-names', immutable['name'], '--availability-zone', immutable['zone'], '--bundle-id', immutable['bundle']])
 
-    # Process ec2 deployments
-    for k,immutable in data['immutables'].items():
+        # Process ec2 deployments
         if (immutable['type'] == "ec2"):
-            subprocess.call(['aws', 'ec2', 'create-instances-from-snapshot', '--instance-snapshot-name', immutable['refer'], '--instance-names', immutable['name'], '--availability-zone', immutable['zone'], '--bundle-id', immutable['bundle']])
+            subprocess.call(['aws', 'ec2', 'run-instances', '--image-id', immutable['refer'], '--instance-names', immutable['name'], '--availability-zone', immutable['zone'], '--bundle-id', immutable['bundle']])
+
 
 # Execute
 if __name__ == '__main__':
