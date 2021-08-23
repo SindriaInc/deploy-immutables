cde9c55ff0d892b873981fd5a4c9d4f825dcd508
lucapitzoi <luca.pitzoi@sindria.org>
2021-08-23 12:28:01 +0200

    Disable debugger for release

--- src/app/main.py
+++ src/app/main.py
@@ -7,15 +7,15 @@ import concurrent.futures
 
 import helpers
 
-# Pycharm pydevd
-PYCHARM_PYDEVD_ENABLED = int(os.getenv('PYCHARM_PYDEVD_ENABLED'))
-PYCHARM_PYDEVD_HOST = str(os.getenv('PYCHARM_PYDEVD_HOST'))
-PYCHARM_PYDEVD_PORT = int(os.getenv('PYCHARM_PYDEVD_PORT'))
-
-# Enable pydevd debugger
-if (PYCHARM_PYDEVD_ENABLED):
-    import pydevd_pycharm
-    pydevd_pycharm.settrace(PYCHARM_PYDEVD_HOST, port=PYCHARM_PYDEVD_PORT, stdoutToServer=True, stderrToServer=True)
+# # Pycharm pydevd
+# PYCHARM_PYDEVD_ENABLED = int(os.getenv('PYCHARM_PYDEVD_ENABLED'))
+# PYCHARM_PYDEVD_HOST = str(os.getenv('PYCHARM_PYDEVD_HOST'))
+# PYCHARM_PYDEVD_PORT = int(os.getenv('PYCHARM_PYDEVD_PORT'))
+#
+# # Enable pydevd debugger
+# if (PYCHARM_PYDEVD_ENABLED):
+#     import pydevd_pycharm
+#     pydevd_pycharm.settrace(PYCHARM_PYDEVD_HOST, port=PYCHARM_PYDEVD_PORT, stdoutToServer=True, stderrToServer=True)
 
 
 # Sample lightsail
